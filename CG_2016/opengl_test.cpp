988da24cb8cb064c989149cb9ba63cf058f37341
Sorae <seokmin@nhnnext.org>
2016-05-07 22:51:34 +0900

    SimpleWindow

---
+++ CG_2016/opengl_test.cpp
@@ -0,0 +1,20 @@
+#include <windows.h>
+#include <gl/glut.h>
+
+void RenderScene(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
+void SetupRC(void)
+{
+   glClearColor(0.0f, 0.0f,1.0f,1.0f);
+}
+void main(void)
+{
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutCreateWindow("CG_2016");
+	glutDisplayFunc(RenderScene);
+	SetupRC();
+	glutMainLoop();
+}
