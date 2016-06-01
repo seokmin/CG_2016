7b7d9fc2750b4271e5c1537a5a8ec85850b54c00
Sorae <seokmin@nhnnext.org>
2016-06-02 02:34:12 +0900

    포인트라이트까지 적용

--- CG_2016/opengl_test.cpp
+++ CG_2016/opengl_test.cpp
@@ -6,6 +6,16 @@ const GLfloat GL_PI = 3.1415;
 
 void SetupRC(void)
 {
+	// ºû
+	GLfloat ambientLight[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+	GLfloat diffuseLight[] = { 0.7f, 0.7f, 0.7f, 1.0f };
+	GLfloat lightPos[] = { 0, 0, -100, 1.0f };
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+
 	glClearColor(0.f, 0.f, 0.f, 1.0f);
 	glColor3f(0.f, 1.f, 0.f);
 	glEnable(GL_DEPTH_TEST);
@@ -43,39 +53,36 @@ void ChangeSize(GLsizei w, GLsizei h)
 void RenderScene(void)
 {
 	static GLfloat fElect1 = 0.f;
+	static GLfloat fElect2 = 0.f;
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	//ÅÂ¾ç
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glTranslatef(0.f, 0.f, -100.f);
 	glColor3ub(255, 0, 0);
 	glutSolidSphere(10.f, 15, 15);
-
+	
+	//Áö±¸
 	glPushMatrix();
 	glRotatef(fElect1, 0.f, 1.f, 0.f);
-	glTranslatef(90.f, 0.f, 0.f);
+	glTranslatef(50.f, 0.f, 0.f);
 	glColor3ub(255, 255, 0);
 	glutSolidSphere(6.f, 15, 15);
-	glPopMatrix();
 
 	glPushMatrix();
-	glRotatef(45.f, 0.f, 1.f, 1.f);
-	glRotatef(fElect1, 0.f, 1.f, 0.f);
-	glTranslatef(-70.f, 0.f, 0.f);
-	glutSolidSphere(6.f, 15, 15);
+	glRotatef(fElect2, 0.f, 1.f, 0.f);
+	glTranslatef(-20.f, 0.f, 0.f);
+	glColor3ub(0, 255, 0);
+	glutSolidSphere(3.f, 15, 15);
 	glPopMatrix();
 
-
-	glPushMatrix();
-	glRotatef(90.f, 0.f, 0.f, 1.f);
-	glRotatef(fElect1, 0.f, 1.f, 0.f);
-	glTranslatef(0.f, 0.f, 60.f);
-	glutSolidSphere(6.f, 15, 15);
-	glPopMatrix();
-
-	fElect1 += 2.f;
+	fElect1 += 0.5f;
+	fElect2 += 2.f;
 	if (fElect1 > 360.f)
 		fElect1 = 0.f;
+	if (fElect2 > 360.f)
+		fElect2 = 0.f;
 
 	glutSwapBuffers();
 }
